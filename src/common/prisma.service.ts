52a8cf5e405db30e9fa5e639600c9fa555201911
fiqriardiansyah <fiqriardian92@gmail.com>
2024-09-02 15:51:10 +0700

    ci: some changes

--- src/common/prisma.service.ts
+++ src/common/prisma.service.ts
@@ -1,9 +1,18 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, OnModuleInit } from "@nestjs/common";
 import { Prisma, PrismaClient } from "@prisma/client";
 
 @Injectable()
-export class PrismaService extends PrismaClient<Prisma.PrismaClientOptions, string> {
+export class PrismaService extends PrismaClient<Prisma.PrismaClientOptions, string> implements OnModuleInit {
     constructor() {
         super();
+    }
+
+    async onModuleInit() {
+        try {
+            await this.$connect();
+            console.log("Database connected! ❤️")
+        } catch (err) {
+            console.log("Failed Connect Database", err);
+        }
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,10 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: "http://localhost:1000",
+    origin: process.env.FE_URL,
     credentials: true
   })
   app.use(cookieParser());
-  await app.listen(process.env.PORT);
+  await app.listen(parseInt(process.env.PORT), process.env.HOST || "localhost");
 }
 bootstrap();
